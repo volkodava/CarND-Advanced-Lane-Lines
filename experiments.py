184f9ea557de0c442a55d2ccece0b698a95ad504
Anatolii Volkodav <volkodav@ngti.nl>
2017-10-16 08:33:02 +0200

    added more filters

--- experiments.py
+++ experiments.py
@@ -139,6 +139,80 @@ def filter_color(image, lower_color_mask, upper_color_mask=None, trg_color_space
     return cv2.bitwise_and(image, image, mask=mask)
 
 
+# Define a function that applies Sobel x or y,
+# then takes an absolute value and applies a threshold.
+def abs_sobel_thresh(img, sobel_kernel=3, orient='x', thresh=(0, 255)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    sbl = cv2.Sobel(gray, cv2.CV_64F, orient == 'x', orient == 'y', ksize=sobel_kernel)
+    # 3) Take the absolute value of the derivative or gradient
+    abs_sbl = np.absolute(sbl)
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sbl = np.uint8(255 * abs_sbl / np.max(abs_sbl))
+    # 5) Create a mask of 1's where the scaled gradient magnitude
+    # is > thresh_min and < thresh_max
+    binary_output = np.zeros_like(scaled_sbl)
+    binary_output[(scaled_sbl >= thresh[0]) & (scaled_sbl <= thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+
+# Define a function that applies Sobel x and y,
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude
+    abs_sobel = np.power(sobelx, 2) + np.power(sobely, 2)
+    magnitude = np.sqrt(abs_sobel)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled_sobel = np.uint8(255 * magnitude / np.max(magnitude))
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+
+# Define a function that applies Sobel x and y,
+# then computes the direction of the gradient
+# and applies a threshold.
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    gradient_direct = np.arctan2(abs_sobely, abs_sobelx)
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(gradient_direct)
+    binary_output[(gradient_direct >= thresh[0]) & (gradient_direct <= thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+
+def gaussian_blur(img, kernel_size):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+
+def moving_average(array, period):
+    ret = np.cumsum(array, dtype=float)
+    ret[period:] = ret[period:] - ret[:-period]
+    return ret[period - 1:] / period
+
+
 if __name__ == "__main__":
     cal_fnames = [path for path in glob.iglob('camera_cal/*.jpg', recursive=True)]
     cal_images, cal_gray_images = read_images(cal_fnames)
@@ -150,9 +224,9 @@ if __name__ == "__main__":
 
     cal_undistorted_image = correct_distortion(cal_test_image, objpoints, imgpoints)
 
-    images_to_show = [cal_test_image, cal_test_image_gray, cal_undistorted_image]
-    labels_to_show = ["Image", "Gray Image", "Undistorted Image"]
-    show_images(images_to_show, labels_to_show, cols=len(images_to_show), title="Calibrate Camera")
+    # images_to_show = [cal_test_image, cal_test_image_gray, cal_undistorted_image]
+    # labels_to_show = ["Image", "Gray Image", "Undistorted Image"]
+    # show_images(images_to_show, labels_to_show, cols=len(images_to_show), title="Calibrate Camera")
 
     print("num of camera calibration images: ", len(cal_images))
     print("camera calibration image shape: ", cal_images[0].shape)
@@ -161,9 +235,9 @@ if __name__ == "__main__":
     example_fnames = ["test_images/test4.jpg", "test_images/test5.jpg"]
     example_images, example_gray_images = read_images(example_fnames)
 
-    show_images(example_images, labels=example_fnames, cols=len(example_images), title="Input Images")
-    show_images(example_gray_images, labels=example_fnames, cols=len(example_gray_images),
-                title="Input Gray Images")
+    # show_images(example_images, labels=example_fnames, cols=len(example_images), title="Input Images")
+    # show_images(example_gray_images, labels=example_fnames, cols=len(example_gray_images),
+    #             title="Input Gray Images")
 
     example_selected_idx = 0
     example_test_image = example_images[example_selected_idx]
@@ -211,5 +285,7 @@ if __name__ == "__main__":
                       example_warped_image, example_filtered_yellow, example_filtered_white, example_combined_filtered]
     labels_to_show = ["Image", "Gray Image", "Undistorted Image", "Masked Image", "Warped Image", "Warped Yellow",
                       "Warped White", "Warped Combined Colors"]
-    show_images(images_to_show, labels=labels_to_show, cols=len(images_to_show) // 2,
-                title="Input Image Transformation")
+    # show_images(images_to_show, labels=labels_to_show, cols=len(images_to_show) // 2,
+    #             title="Input Image Transformation")
+
+    # TODO: Apply sobel filters
