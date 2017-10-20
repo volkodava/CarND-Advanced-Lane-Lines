b3dbe395b9c6745f1509c8c8a0c58e7325c3a5d8
Anatolii Volkodav <volkodav@ngti.nl>
2017-10-21 00:14:50 +0200

    implemented first sliding windows

--- experiments.py
+++ experiments.py
@@ -1,6 +1,5 @@
 import glob
 import io
-from functools import partial
 
 import cv2
 import matplotlib.pyplot as plt
@@ -355,21 +354,27 @@ def tag_video(finput, foutput, processor, subclip_secs=None):
     out_clip.write_videofile(foutput, audio=False)
 
 
-def debug_image(gray_image, num_of_bins=50):
-    height, width = gray_image.shape[:2]
-    half_image = gray_image[height // 2:, :]
+def calc_avg_vertical(image, num_of_bins=50):
+    height, width = image.shape[:2]
+    half_image = image[height // 2:, :]
 
     bin_size = width // num_of_bins
     avg_vertical = np.mean(half_image, axis=0)
     avg_vertical = moving_average(avg_vertical, bin_size)
+
+    return avg_vertical, half_image
+
+
+def debug_image(gray_image, num_of_bins=50):
+    avg_vertical, half_image = calc_avg_vertical(gray_image, num_of_bins)
 
     plt.subplot(2, 1, 1)
     plt.imshow(half_image, cmap='gray')
     plt.axis('off')
     plt.subplot(2, 1, 2)
     plt.plot(avg_vertical, 'b')
-    plt.ylabel('# vertical pixels')
-    plt.xlim(0, width)
+    plt.ylabel('Counts')
+    plt.ylabel('Pixel Position')
     plt.tight_layout()
 
     buf = io.BytesIO()
@@ -418,6 +423,82 @@ def combine_3_images(main, first, second):
         cv2.resize(second, (second_size_width, second_size_height))
 
     return result_image
+
+
+def fit_polynomial(binary_warped, histogram):
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = histogram.shape[0] // 2
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+                      (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+                      (0, 255, 0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    return left_fit, right_fit, out_img
 
 
 if __name__ == "__main__":
@@ -529,20 +610,38 @@ if __name__ == "__main__":
     main_thresh_image = np.uint8(apply_threshold(main_warped_image) * 255)
 
     # debug_image(main_thresh_image)
-    thresh_debug_image = debug_image(main_thresh_image)
-
-    combined_image = combine_3_images(main_image, grayscale_ro_rgb(main_warped_image),
-                                      thresh_debug_image)
-
-    plt.imshow(main_thresh_image, cmap="gray")
-    plt.imshow(combined_image)
-    plt.show()
-
-    tag_video("project_video.mp4", "out_project_video.mp4",
-              partial(process_image, objpoints=objpoints, imgpoints=imgpoints), subclip_secs=(36, 42))
+    # thresh_debug_image = debug_image(main_thresh_image)
+    #
+    # combined_image = combine_3_images(main_image, grayscale_ro_rgb(main_warped_image),
+    #                                   thresh_debug_image)
+    #
+    # plt.imshow(main_thresh_image, cmap="gray")
+    # plt.imshow(combined_image)
+    # plt.show()
+    #
+    # tag_video("project_video.mp4", "out_project_video.mp4",
+    #           partial(process_image, objpoints=objpoints, imgpoints=imgpoints), subclip_secs=(36, 42))
     # tag_video("project_video.mp4", "out_project_video.mp4",
     #           partial(process_image, objpoints=objpoints, imgpoints=imgpoints))
     # tag_video("challenge_video.mp4", "out_challenge_video.mp4",
     #           partial(process_image, objpoints=objpoints, imgpoints=imgpoints))
     # tag_video("harder_challenge_video.mp4", "out_harder_challenge_video.mp4",
     #           partial(process_image, objpoints=objpoints, imgpoints=imgpoints))
+
+    height, width = main_thresh_image.shape[:2]
+
+    avg_vertical, half_image = calc_avg_vertical(main_thresh_image)
+
+    left_fit, right_fit, out_img = fit_polynomial(main_thresh_image, avg_vertical)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, main_thresh_image.shape[0] - 1, main_thresh_image.shape[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, width)
+    plt.ylim(height, 0)
+    plt.show()
