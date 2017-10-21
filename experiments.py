90b362297e13e74f4eee80c4ef64e135654f51e6
Anatolii Volkodav <volkodav@ngti.nl>
2017-10-21 17:13:31 +0200

    implemented draw lane space method

--- experiments.py
+++ experiments.py
@@ -566,6 +566,29 @@ def calculate_radius(leftx, lefty, rightx, righty, ploty):
     return left_curverad, right_curverad
 
 
+def draw_lane_space(image, warped, Minv, left_fitx, right_fitx):
+    height, width = image.shape[:2]
+
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (width, height))
+    # Combine the result with the original image
+    result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+
+    return result
+
+
 if __name__ == "__main__":
     cal_fnames = [path for path in glob.iglob('camera_cal/*.jpg', recursive=True)]
     cal_images, cal_gray_images = read_images(cal_fnames)
@@ -752,12 +775,19 @@ if __name__ == "__main__":
     plt.plot(right_fitx, ploty, color='yellow')
     plt.xlim(0, width)
     plt.ylim(height, 0)
-    plt.show()
+    # plt.show()
 
     left_radius, right_radius = \
         calculate_radius(leftx, lefty, rightx, righty, ploty)
 
     print("left_radius: ", left_radius, "m")
     print("right_radius: ", right_radius, "m")
+
+    main_lane_space_image = draw_lane_space(main_image, main_thresh_image, Minv, left_fitx, right_fitx)
+
+    plt.close()
+    plt.imshow(main_lane_space_image)
+    plt.axis('off')
+    plt.show()
 
     # sample_code()
